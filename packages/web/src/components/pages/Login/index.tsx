74b1d2d7270ba58464c84b5256a5a275d8de6ac1
Andres Mauricio Ramirez <amrp0219@gmail.com>
2021-10-06 19:29:53 -0500

    yarn workspaces and react

---
+++ packages/web/src/components/pages/Login/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button } from 'common-components';
+
+export const Login = (): JSX.Element => {
+  return (
+    <div>
+      Login a
+      <Button />
+    </div>
+  );
+};
